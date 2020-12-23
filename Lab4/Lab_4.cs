5d775934c43a5966404ce5b433c06b06b5ce7e24
NikKnyshev <55587189+NikKnyshev@users.noreply.github.com>
2020-12-23 11:07:09 +0300

    Add files via upload

--- Lab4/Lab_4.cs
+++ Lab4/Lab_4.cs
@@ -1,78 +1,24 @@
 using System;
 
-namespace Lab4
+namespace OOP
 {
-    class B
-    {
-        public float[] arr = new [] {2.5f, 5.55f, 23.2f, 11.2f, 16.16f};
-        public int[] arr2 = new [] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        
-        public float this[int ind1]
-	    {
-		    get{ return arr[ind1]; }
-		    set{ arr[ind1] = value; }
-	    }
-        public int this[int ind2, int plug] 
-	    {
-		    get{ return arr2[ind2]; }
-		    set{ arr2[ind2] = value; }
-	    }
-
-    }
-
-    class C<T>    
-    {
-        public T variable = default(T);
-        public static string banner = "╔═══════════════════════════════════╗\n" +
-                                      "║             HELLO WORLD           ║\n" +
-                                      "╚═══════════════════════════════════╝\n";
-
-        public void PrintBanner()
-        {
-            Console.WriteLine(banner);
-        }
-    }
-    
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            B bObj = new B();
-
-            for (int i = 0; i < bObj.arr.Length; i++)
-            {
-                Console.Write($"{bObj[i]}\t");
-            }
-            Console.WriteLine();
-            for (int i = 0; i < bObj.arr2.Length; i++)
-            {
-                Console.Write($"{bObj[i,1]}\t");
-            }
-            Console.WriteLine();
-            
-            
-            C<string> cObj_1 = new C<string>();
-            C<int> cObj_2 = new C<int>();       
-
-            Console.WriteLine("C<int>.banner:");
-            Console.WriteLine(C<int>.banner);
-
-            cObj_1.variable = "Hello world!";
-            cObj_2.variable = 255;
-            
-            Console.WriteLine("cObj_1.PrintBanner():");
-            cObj_1.PrintBanner();
-            Console.WriteLine("cObj_2.PrintBanner():");
-            cObj_2.PrintBanner();
-            
-            C<int>.banner = "╔═══════════════════╗\n" +
-                            "║ New banner saved! ║\n" + 
-                            "╚═══════════════════╝\n";
-            
-            Console.WriteLine("cObj_1.PrintBanner():");
-            cObj_1.PrintBanner();
-            Console.WriteLine("cObj_2.PrintBanner():");
-            cObj_2.PrintBanner();
-        }
-    }
+class Program
+{
+static void Main(string[] args)
+{
+B classB = new B(3, 4);
+C<string> classC_string = new C<string>();
+C<int> classC_int = new C<int>();
+Console.WriteLine("Первый массив:");
+foreach(var ind in classB.arrayClass1)
+{
+Console.WriteLine(ind);
+}
+Console.WriteLine("Второй массив:");
+foreach (var ind in classB.arrayClass2)
+{
+Console.WriteLine(ind);
+}
+}
+}
 }
